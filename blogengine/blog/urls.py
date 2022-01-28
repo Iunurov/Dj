025f33346115743d64e80cde60d9998b43b360f5
Iunurov <rashid010196@gmail.com>
2022-01-28 21:35:09 +0600

    pat 6

--- blogengine/blog/urls.py
+++ blogengine/blog/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .views import *
 
 
-
-
 urlpatterns = [
     path('', post_list, name='post_list_url'),
     path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
     path('tags/', tags_list, name='tags_list_url'),
-    path('tag/<str:slug>', TagDetail.as_view(), name='tag-detail_url')
+    path('tag/create/', TagCreate.as_view(), name='tag_create_url'),
+    path('tag/<str:slug>/', TagDetail.as_view(), name='tag-detail_url'),
+
 ]

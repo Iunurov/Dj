3905b34c70dfbd74457ed4926dff9d8720a7746d
Iunurov <rashid010196@gmail.com>
2022-01-28 14:55:04 +0600

    pat 2

--- blogengine/blog/views.py
+++ blogengine/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 def post_list(request):
-    return HttpResponse('<h1>Hello word</h1>')
+    n = ['Oleg', 'Masha', 'Olja', 'Ksu']
+    return render(request, 'blog/index.html', context={'names': n})
